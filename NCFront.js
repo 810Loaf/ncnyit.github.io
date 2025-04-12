62d252deb0501835bdc87176526881c7efcca59b
810Loaf <137736680+810Loaf@users.noreply.github.com>
2025-04-11 20:43:56 -0400

    Create NCFront.js

---
+++ NCFront.js
@@ -0,0 +1,32 @@
+const reportButton = document.getElementById('reportButton');
+const footerText = document.getElementById('footerText');
+
+reportButton.addEventListener('mousedown', function() {
+  this.classList.add('clicked');
+});
+reportButton.addEventListener('mouseup', function() {
+  this.classList.remove('clicked');
+});
+reportButton.addEventListener('mouseleave', function() {
+  this.classList.remove('clicked');
+});
+
+footerText.addEventListener('mousedown', function() {
+  this.classList.add('clicked');
+});
+footerText.addEventListener('mouseup', function() {
+  this.classList.remove('clicked');
+});
+footerText.addEventListener('mouseleave', function() {
+  this.classList.remove('clicked');
+});
+
+reportButton.addEventListener('click', goToForm);
+footerText.addEventListener('click', goToAbout);
+
+function goToForm() {
+  window.location.href = "form.html"; //Replace form page
+}
+function goToAbout() {
+  window.location.href = "about.html"; //Replace about page
+}
